d30436cdbde86eddfae8009afb2399dd746420a2
kidde60 <kgeorgewilliam60@gmail.com>
2022-07-18 13:31:19 +0300

    Add navlist

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function myFunction(x) {
     x.classList.toggle("change");
 }
+let mobile = document.querySelector('.mobile-view');
+let navmenu = document.querySelector('.mobile');
+navmenu.addEventListener("click", function () {
+    if (mobile.style.display === "block") {
+        mobile.style.display = "none";
+    } else {
+        mobile.style.display = "block";
+    }
+
+});
