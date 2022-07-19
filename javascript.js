0489fc6e677241b6ad379a303112fcde01af4dac
kidde60 <kgeorgewilliam60@gmail.com>
2022-07-19 11:44:29 +0300

    Add menu

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+let links = document.q
