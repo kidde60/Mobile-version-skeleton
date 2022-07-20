daaaf47c464e6b1b2f034d3929225b4326eac495
kidde60 <kgeorgewilliam60@gmail.com>
2022-07-20 22:51:23 +0300

    Edit form validation

--- script.js
+++ script.js
@@ -212,17 +212,16 @@ const validationMessage = document.querySelector('#error');
 validationMessage.style.color = '#ff0000';
 validationMessage.style.fontSize = '15px';
 validationMessage.style.padding = '4px';
-contactForm.addEventListener("submit", (e) => {
+contactForm.addEventListener('submit', (e) => {
   let isValid = false;
-  if(email.value === email.value.toLowerCase()) {
+  if (email.value === email.value.toLowerCase()) {
     isValid = true;
   }
-  if(isValid === false) {
+  if (isValid === false) {
     e.preventDefault();
-    validationMessage.innerText = 'Please type lowercase text';
+    validationMessage.innerText = 'Email must contain only lowercase letters';
     setTimeout(() => {
-      validationMassege.textContent = '';
+      validationMessage.textContent = '';
     }, 5000);
   }
-})
-
+});
