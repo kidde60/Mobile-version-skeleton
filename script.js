db609ba6668ed6a584c6ab4586e9c0fed9fd5d58
kidde60 <kgeorgewilliam60@gmail.com>
2022-07-21 11:52:42 +0300

    Add navbar

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
+  document.getElementById('myNav').style.height = '100%';
+} openNav();
 
 /* Close */
 function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
-let overlay = document.querySelector('#myNav');
-let links = document.querySelector('.menu-link');
-let links1 = document.querySelector('.menu-link1');
-let links2 = document.querySelector('.menu-link2');
-links.addEventListener("click", () => {
-    if (overlay.style.display === 'block') {
-        overlay.style.height = "100%";
-    } else {
-        overlay.style.height = "0%";
-    }
-
+  document.getElementById('myNav').style.height = '0%';
+} closeNav();
+const overlay = document.querySelector('#myNav');
+const links = document.querySelector('.menu-link');
+const links1 = document.querySelector('.menu-link1');
+const links2 = document.querySelector('.menu-link2');
+links.addEventListener('click', () => {
+  if (overlay.style.display === 'block') {
+    overlay.style.height = '100%';
+  } else {
+    overlay.style.height = '0%';
+  }
 });
-links1.addEventListener("click", () => {
-    if (overlay.style.display === 'block') {
-        overlay.style.height = "100%";
-    } else {
-        overlay.style.height = "0%";
-    }
-
+links1.addEventListener('click', () => {
+  if (overlay.style.display === 'block') {
+    overlay.style.height = '100%';
+  } else {
+    overlay.style.height = '0%';
+  }
 });
-links2.addEventListener("click", () => {
-    if (overlay.style.display === 'block') {
-        overlay.style.height = "100%";
-    } else {
-        overlay.style.height = "0%";
-    }
-
+links2.addEventListener('click', () => {
+  if (overlay.style.display === 'block') {
+    overlay.style.height = '100%';
+  } else {
+    overlay.style.height = '0%';
+  }
 });
