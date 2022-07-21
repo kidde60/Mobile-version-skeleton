f0f8d130daddbf4c0c3aeb36fa5a30af4fa67f57
kidde60 <kgeorgewilliam60@gmail.com>
2022-07-21 10:10:57 +0300

    add html

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-let checkbox = document.querySelector('.navigation__checkbox');
-let menu = document.querySelector('.mobile-view');
-checkbox.addEventListener("click", function () {
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+let overlay = document.querySelector('#myNav');
+let links = document.querySelector('.menu-link');
+let links1 = document.querySelector('.menu-link1');
+let links2 = document.querySelector('.menu-link2');
+links.addEventListener("click", () => {
+    if (overlay.style.display === 'block') {
+        overlay.style.height = "100%";
     } else {
-        menu.style.display = 'block';
+        overlay.style.height = "0%";
     }
+
+});
+links1.addEventListener("click", () => {
+    if (overlay.style.display === 'block') {
+        overlay.style.height = "100%";
+    } else {
+        overlay.style.height = "0%";
+    }
+
+});
+links2.addEventListener("click", () => {
+    if (overlay.style.display === 'block') {
+        overlay.style.height = "100%";
+    } else {
+        overlay.style.height = "0%";
+    }
+
 });
