a5cec6471012f53b7e1c940856cd9ca4d42ae0dc
kidde60 <kgeorgewilliam60@gmail.com>
2022-07-21 09:50:36 +0300

    Add nav actions

--- javascript.js
+++ javascript.js
@@ -6,5 +6,31 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+let overlay = document.querySelector('#myNav');
+let links = document.querySelector('.menu-link');
+let links1 = document.querySelector('.menu-link1');
+let links2 = document.querySelector('.menu-link2');
+links.addEventListener("click", () => {
+    if (overlay.style.display === 'block') {
+        overlay.style.height = "100%";
+    } else {
+        overlay.style.height = "0%";
+    }
 
-let links = document.q
+});
+links1.addEventListener("click", () => {
+    if (overlay.style.display === 'block') {
+        overlay.style.height = "100%";
+    } else {
+        overlay.style.height = "0%";
+    }
+
+});
+links2.addEventListener("click", () => {
+    if (overlay.style.display === 'block') {
+        overlay.style.height = "100%";
+    } else {
+        overlay.style.height = "0%";
+    }
+
+});
